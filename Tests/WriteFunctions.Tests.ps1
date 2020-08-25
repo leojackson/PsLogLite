cf17217fdcea1f62d187720a60315a5f731ca92a
Leo Jackson <leo.jackson@gmail.com>
2020-08-25 02:17:02 -0700

    Refactored Pester tests to improve code maint.

--- Tests/WriteFunctions.Tests.ps1
+++ Tests/WriteFunctions.Tests.ps1
@@ -9,6 +9,10 @@ Describe -Name "Write-* functions" {
         Set-LogLevel -Level "Debug"
     }
 
+    AfterEach {
+        Reset-LogPath
+    }
+
     Context "Write-Debug" {
         BeforeAll {
             $TestLogFile = "$TestLogPath\Write-Debug.log"
@@ -21,20 +25,15 @@ Describe -Name "Write-* functions" {
             Write-Debug -Message "This is a debug message." 5>&1 | Should -Be "This is a debug message."
             $Global:DebugPreference = $DebugTemp
         }
-        It "writes to a log file" {
-            Write-Debug -Message "Test" | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*DEBUG - Test*"
-        }
-        It "fails when no message is provided" {
-            Write-Debug -Message $null | Should -BeNullOrEmpty
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*CRIT - FAILED TO LOG DEBUG MESSAGE*"
-        }
-        It "resets the output buffer when -OutBuffer is specified" {
-            Write-Debug -Message "TestBuffer" -OutBuffer 2
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*DEBUG - TestBuffer*"
-        }
-        AfterAll {
-            Reset-LogPath
+        # These tests go through use cases for different input types and param sets, and tests for the expected value in the log
+        It "<Description>" -TestCases @(
+            @{ Description = "writes to a log file"; Params = @{ Message = "Test" }; OutString = "*DEBUG - Test*" }
+            @{ Description = "fails when no message is provided"; Params = @{ Message = $null; ErrorAction = "SilentlyContinue" }; OutString = "*CRIT - FAILED TO LOG DEBUG MESSAGE*" }
+            @{ Description = "resets the output buffer when -OutBuffer is specified"; Params = @{ Message = "TestBuffer"; OutBuffer = 2 }; OutString = "*DEBUG - TestBuffer*" }
+        ) {
+            param($Params,$OutString)
+            Write-Debug @Params *>&1 | Out-Null
+            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly $OutString
         }
     }
     
@@ -47,33 +46,25 @@ Describe -Name "Write-* functions" {
         It "writes only to the Error stream" {
             Write-Error -Message "This is an error message." -ErrorAction Continue 2>&1 | Should -BeLikeExactly "This is an error message."
         }
-        It "writes to a log file when -Message specified" {
-            Write-Error -Message "Test" -ErrorAction SilentlyContinue | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*ERROR - Test*"
-        }
-        It "writes to a log file when -Exception specified" {
-            $Except = [System.Exception]::new("Test")
-            $Except.Source = "PsLogLite"
-            Write-Error -Exception $Except -ErrorAction SilentlyContinue | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*ERROR - Test*"
-        }
-        It "writes to a log file when -ErrorRecord specified" {
-            $Except = [System.Exception]::new("Test")
-            $Except.Source = "PsLogLite"
-            $ErrorRecord = [System.Management.Automation.ErrorRecord]::new($Except,"TestError","WriteError",$null)
-            Write-Error -ErrorRecord $ErrorRecord -ErrorAction SilentlyContinue | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*ERROR - Test*"
-        }
-        It "fails when no message is provided" {
-            Write-Error -Message $null -ErrorAction SilentlyContinue | Should -BeNullOrEmpty
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*CRIT - FAILED TO LOG ERROR MESSAGE*"
-        }
-        It "resets the output buffer when -OutBuffer is specified" {
-            Write-Error -Message "TestBuffer" -OutBuffer 2 -ErrorAction SilentlyContinue
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*ERROR - TestBuffer*"
-        }
-        AfterAll {
-            Reset-LogPath
+        # These tests go through use cases for different input types and param sets, and tests for the expected value in the log
+        It "<Description>" -TestCases @(
+            @{ Description = "writes to a log file when -Message specified"; Params = @{Message = "Test"}; OutString = "*ERROR - Test*" }
+            @{ Description = "writes to a log file when -Exception specified"; Params = @{Exception = $(
+                    $Except = [System.Exception]::new("Test")
+                    $Except.Source = "PsLogLite"
+                    $Except
+                )}; OutString = "*ERROR - Test*" }
+            @{ Description = "writes to a log file when -ErrorRecord specified"; Params = @{ErrorRecord = $(
+                    $Except = [System.Exception]::new("Test")
+                    $Except.Source = "PsLogLite"
+                    [System.Management.Automation.ErrorRecord]::new($Except,"TestError","WriteError",$null)
+                )}; OutString = "*ERROR - Test*" }
+            @{ Description = "fails when no message is provided"; Params = @{Message = $null}; OutString = "*CRIT - FAILED TO LOG ERROR MESSAGE*" }
+            @{ Description = "resets the output buffer when -OutBuffer is specified"; Params = @{Message = "TestBuffer"; OutBuffer = 2}; OutString = "*ERROR - TestBuffer*" }
+        ) {
+            param($Params,$OutString)
+            Write-Error -ErrorAction SilentlyContinue @Params | Out-Null
+            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly $OutString
         }
     }
 
@@ -86,20 +77,15 @@ Describe -Name "Write-* functions" {
         It "writes to the Information stream" {
             Write-Host -Object "This is a host message." 6>&1 | Should -Be "This is a host message."
         }
-        It "writes to a log file" {
-            Write-Host -Object "Test" 6>&1 | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*HOST - Test*"
-        }
-        It "fails when no message is provided" {
-            Write-Host -Object $null -ErrorAction SilentlyContinue | Should -BeNullOrEmpty
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*CRIT - FAILED TO LOG HOST MESSAGE*"
-        }
-        It "resets the output buffer when -OutBuffer is specified" {
-            Write-Host -Object "TestBuffer" -OutBuffer 2 6>&1 | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*HOST - TestBuffer*"
-        }
-        AfterAll {
-            Reset-LogPath
+        # These tests go through use cases for different input types and param sets, and tests for the expected value in the log
+        It "<Description>" -TestCases @(
+            @{ Description = "writes to a log file"; Params = @{ Object = "Test" }; OutString = "*HOST - Test*" }
+            @{ Description = "fails when no message is provided"; Params = @{ Object = $null; ErrorAction = "SilentlyContinue" }; InString = $null; OutString = "*CRIT - FAILED TO LOG HOST MESSAGE*" }
+            @{ Description = "resets the output buffer when -OutBuffer is specified"; Params = @{ Object = "TestBuffer"; OutBuffer = 2 }; InString = "TestBuffer"; OutString = "*HOST - TestBuffer*" }
+        ) {
+            param($Params,$OutString)
+            Write-Host @Params *>&1 | Out-Null
+            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly $OutString
         }
     }
 
@@ -112,20 +98,15 @@ Describe -Name "Write-* functions" {
         It "writes only to the Information stream" {
             Write-Information -MessageData "This is an information message." -InformationAction Continue 6>&1 | Should -Be "This is an information message."
         }
-        It "writes to a log file" {
-            Write-Information -MessageData "Test" | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*INFO - Test*"
-        }
-        It "fails when no message is provided" {
-            Write-Information -MessageData "" | Should -BeNullOrEmpty
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*CRIT - FAILED TO LOG INFO MESSAGE*"
-        }
-        It "resets the output buffer when -OutBuffer is specified" {
-            Write-Information -MessageData "TestBuffer" -OutBuffer 2 6>&1 | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*INFO - TestBuffer*"
-        }
-        AfterAll {
-            Reset-LogPath
+        # These tests go through use cases for different input types and param sets, testing for the expected value in the log
+        It "<Description>" -TestCases @(
+            @{ Description = "writes to a log file"; Params = @{ MessageData = "Test" }; OutString = "*INFO - Test*" }
+            @{ Description = "fails when no message is provided"; Params = @{ MessageData = ""; ErrorAction = "SilentlyContinue" }; OutString = "*CRIT - FAILED TO LOG INFO MESSAGE*" }
+            @{ Description = "resets the output buffer when -OutBuffer is specified"; Params = @{ MessageData = "TestBuffer"; OutBuffer = 2 }; OutString = "*INFO - TestBuffer*" }
+        ) {
+            param($Params,$OutString)
+            Write-Information @Params *>&1 | Out-Null
+            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly $OutString
         }
     }
 
@@ -138,20 +119,15 @@ Describe -Name "Write-* functions" {
         It "writes only to the Success stream" {
             Write-Output -InputObject "This is an output message." | Should -Be "This is an output message."
         }
-        It "writes to a log file" {
-            Write-Output -InputObject "Test" | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*OUTPUT - Test*"
-        }
-        It "fails when no message is provided" {
-            Write-Output -InputObject $null | Should -BeNullOrEmpty
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*CRIT - FAILED TO LOG OUTPUT MESSAGE*"
-        }
-        It "resets the output buffer when -OutBuffer is specified" {
-            Write-Output -InputObject "TestBuffer" -OutBuffer 2 | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*OUTPUT - TestBuffer*"
-        }
-        AfterAll {
-            Reset-LogPath
+        # These tests go through use cases for different input types and param sets, and tests for the expected value in the log
+        It "<Description>" -TestCases @(
+            @{ Description = "writes to a log file"; Params = @{ InputObject = "Test" }; OutString = "*OUTPUT - Test*" }
+            @{ Description = "fails when no message is provided"; Params = @{ InputObject = $null; ErrorAction = "SilentlyContinue"}; OutString = "*CRIT - FAILED TO LOG OUTPUT MESSAGE*" }
+            @{ Description = "resets the output buffer when -OutBuffer is specified"; Params = @{ InputObject = "TestBuffer"; OutBuffer = 2 }; OutString = "*OUTPUT - TestBuffer*" }
+        ) {
+            param($Params,$OutString)
+            Write-Output @Params *>&1 | Out-Null
+            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly $OutString
         }
     }
 
@@ -167,20 +143,15 @@ Describe -Name "Write-* functions" {
             Write-Verbose -Message "This is a verbose message." 4>&1 | Should -Be "This is a verbose message."
             $Global:VerbosePreference = $VerboseTemp
         }
-        It "writes to a log file" {
-            Write-Verbose -Message "Test" | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*VERBOSE - Test*"
-        }
-        It "fails when no message is provided" {
-            Write-Verbose -Message $null | Should -BeNullOrEmpty
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*CRIT - FAILED TO LOG VERBOSE MESSAGE*"
-        }
-        It "resets the output buffer when -OutBuffer is specified" {
-            Write-Verbose -Message "TestBuffer" -OutBuffer 2 | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*VERBOSE - TestBuffer*"
-        }
-        AfterAll {
-            Reset-LogPath
+        # These tests go through use cases for different input types and param sets, and tests for the expected value in the log
+        It "<Description>" -TestCases @(
+            @{ Description = "writes to a log file"; Params = @{ Message = "Test" }; OutString = "*VERBOSE - Test*" }
+            @{ Description = "fails when no message is provided"; Params = @{ Message = $null; ErrorAction = "SilentlyContinue" }; OutString = "*CRIT - FAILED TO LOG VERBOSE MESSAGE*" }
+            @{ Description = "resets the output buffer when -OutBuffer is specified"; Params = @{  Message = "TestBuffer"; OutBuffer = 2}; OutString = "*VERBOSE - TestBuffer*" }
+        ) {
+            param($Params,$OutString)
+            Write-Verbose @Params *>&1 | Out-Null
+            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly $OutString
         }
     }
 
@@ -193,20 +164,15 @@ Describe -Name "Write-* functions" {
         It "writes only to the Warning stream" {
             Write-Warning -Message "This is an warning message." -WarningAction Continue 3>&1 | Should -BeLikeExactly "This is an warning message."
         }
-        It "writes to a log file" {
-            Write-Warning -Message "Test" -WarningAction SilentlyContinue | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*WARN - Test*"
-        }
-        It "fails when no message is provided" {
-            Write-Warning -Message $null -WarningAction SilentlyContinue | Should -BeNullOrEmpty
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*CRIT - FAILED TO LOG WARN MESSAGE*"
-        }
-        It "resets the output buffer when -OutBuffer is specified" {
-            Write-Warning -Message "TestBuffer" -OutBuffer 2 3>&1 | Out-Null
-            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly "*WARN - TestBuffer*"
-        }
-        AfterAll {
-            Reset-LogPath
+        # These tests go through use cases for different input types and param sets, and tests for the expected value in the log
+        It "<Description>" -TestCases @(
+            @{ Description = "writes to a log file"; Params = @{ Message = "Test" }; OutString = "*WARN - Test*" }
+            @{ Description = "fails when no message is provided"; Params = @{ Message = $null; ErrorAction = "SilentlyContinue" }; OutString = "*CRIT - FAILED TO LOG WARN MESSAGE*" }
+            @{ Description = "resets the output buffer when -OutBuffer is specified"; Params = @{ Message = "TestBuffer"; OutBuffer = 2 }; OutString = "*WARN - TestBuffer*" }
+        ) {
+            param($Params,$OutString)
+            Write-Warning @Params *>&1 | Out-Null
+            Get-Content -Path $(Get-LogPath) -Raw | Should -BeLikeExactly $OutString
         }
     }
 
